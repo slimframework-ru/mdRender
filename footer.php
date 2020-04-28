0490a61a1f4b962329736d845fe6bbdb22fac71e
bad4iz <develop@bad4iz.ru>
2020-04-28 15:32:48 +0400

    first commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<p>Перевод
+  <a href="">bad4iz</a>
+</p>
+<p>
+  Созданный и поддерживаемый  <br/>
+  <a href="http://joshlockhart.com">Josh Lockhart</a>,
+  <a href="http://www.silentworks.co.uk" target="_blank">Andrew Smith</a>,
+  <a href="http://akrabat.com/" target="_blank">Rob Allen</a>, и командой
+  <a href="https://github.com/orgs/slimphp/people" target="_blank">Slim Framework</a>
+</p>
