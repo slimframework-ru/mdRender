bce89ccfea77d8ae5cdacda5bc8ae1ba11ea51db
Ivan Dudarev <ivan@ddrv.ru>
2020-06-03 20:36:16 +0700

    add config

---
+++ config/config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'metrika' => null,
+    'mdRepository' => 'https://github.com/slimframework-ru/slim.ru',
+);
--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+$config = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
+
 $Parsedown = new Parsedown();
 
 $title = '';
