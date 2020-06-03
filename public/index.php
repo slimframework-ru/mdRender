3c577a5e0912827a6c5ae0712e67e1cb4d233ebd
Ivan Dudarev <ivan@ddrv.ru>
2020-06-04 01:24:22 +0700

    new template

--- public/index.php
+++ public/index.php
@@ -21,10 +21,7 @@ if (array_key_exists('repository', $config) && !empty($config['repository'])) {
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = preg_replace('/(^\/|\.html$)/ui', '', $path);
-if (!$path) {
-    $path = 'index';
-}
-
+$current = '/' . $path;
 $version = null;
 preg_match('/^v(?<ver>\d+)($|\/)/ui', $path, $matches);
 if (array_key_exists('ver', $matches)) {
@@ -32,18 +29,23 @@ if (array_key_exists('ver', $matches)) {
 }
 
 if (!$version) {
-    header('Location: /v3/index');
+    header('Location: /v3');
     die;
 }
 
-$url = $repository . '/blob/master/' . $path . '.md';
-$path = str_replace('/', DIRECTORY_SEPARATOR, $path);
-$file = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'md' . DIRECTORY_SEPARATOR . $path . '.md';
+$dir = implode(DIRECTORY_SEPARATOR, array(dirname(__DIR__), 'md',  str_replace('/', DIRECTORY_SEPARATOR, $path)));
+
+if (is_dir($dir)) {
+    $file = $dir . DIRECTORY_SEPARATOR . 'index.md';
+    $url = $repository . '/blob/master/' . $path . '/index.md';
+} else {
+    $file = $dir . '.md';
+    $url = $repository . '/blob/master/' . $path . '.md';
+}
 
 if (file_exists($file)) {
     $parser = new Parsedown();
     $md = file_get_contents($file);
-
     $pos = strpos($md, '---', 10);
     $head = substr($md, 0, $pos);
     $md = substr($md, $pos + 3);
@@ -61,4 +63,17 @@ if ($title) {
 };
 $title .= 'Русская документация Slim Framework';
 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'new.php';
+$sidebar = [
+    'Get Started' => [
+        '/v3' => 'Home',
+        '/v3/start/installation' => 'Installation',
+    ],
+    'Tutorial' => [
+        '/v3/tutorial/first-app' => 'First Application',
+    ],
+    'The Application' => [
+        '/v3/objects/application' => 'Overview',
+    ],
+];
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'index.php';
